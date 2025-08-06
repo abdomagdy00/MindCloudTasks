56fcef9c4fa3c2d61774169b9942288f5380ce2c
abdomagdy00 <108371138+abdomagdy00@users.noreply.github.com>
2025-08-06 18:05:27 +0300

    problem2[B - Anton and Digits]

--- main.cpp
+++ main.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
-#include <iomanip>
+#include <algorithm>
 
 int main() {
-    int n;
-    std::cout<<"Enter Number Of n\n";
-    std::cin >> n;
+    // Read the number of available digits
+    int k2, k3, k5, k6;
+    std::cout<<"Enter The Number Of K2\n";
+    std::cin >> k2 ;
+    std::cout<<"Enter The Number Of K3\n";
+    std::cin>> k3 ;
+    std::cout<<"Enter The Number Of K5\n";
+    std::cin>> k5 ;
+    std::cout<<"Enter The Number Of K6\n";
+    std::cin>> k6;
 
-    double sum_of_percentages = 0.0;
-    for (int i = 0; i < n; ++i) {
-        double p;
-        std::cout<<"Enter percentage["<<i+1<<"] \n";
-        std::cin >> p;
-        sum_of_percentages += p;
-    }
+    long long total_sum = 0;
+    int num_256 = std::min({k2, k5, k6});
+    total_sum += num_256 * 256;
 
-    double final_percentage = sum_of_percentages / n;
-    
-    std::cout << std::fixed << std::setprecision(12) << final_percentage << std::endl;
+    // Update the count of digit 2, as it was used to form 256s
+    k2 -= num_256;
+    int num_32 = std::min(k2, k3);
+    total_sum += num_32 * 32;
+    std::cout<<"-------------------\n";
+    std::cout <<"Total Sum= " << total_sum << std::endl;
 
     return 0;
 }
